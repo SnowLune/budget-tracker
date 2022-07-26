6252b6d3ae6a34b5089f1fe9e70c462cc55b5a1f
Luna R <snowluna@protonmail.com>
2022-07-26 19:39:11 -0400

    add event listeners to service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -19,3 +19,35 @@ self.addEventListener("install", function(e) {
          })
    )
 })
+
+// Listen for service worker activation
+self.addEventListener("activate", function(e) {
+   e.waitUntil(
+      caches.keys()
+         .then(function(keyList) {
+            let cacheKeepList = keyList.filter(function(key) {
+               return key.indexOf(APP_PREFIX);
+            })
+
+            cacheKeepList.push(CACHE_NAME);
+
+            return Promise.all(keyList.map(function(key, i) {
+               if (cacheKeepList.indexOf(key) === -1) {
+                  console.log("Deleting cache: " + keyList[i]);
+                  return caches.delete(keyList[i]);
+               }
+            }))
+         })
+   )
+})
+
+self.addEventListener("fetch", function(e) {
+   console.log("Fetch request: " + e.request.url)
+   e.respondWith(
+      caches.match(e.request)
+         .then(function(request) {
+            // Check with we already have the cached files
+            return request || fetch(e.request)
+         })
+   )
+})
