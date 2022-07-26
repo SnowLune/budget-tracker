85d85102170afdca07ab492a261ad119a6ba3fa4
Luna R <snowluna@protonmail.com>
2022-07-26 18:41:26 -0400

    implement indexeddb

--- public/js/idb.js
+++ public/js/idb.js
@@ -22,3 +22,39 @@ function saveRecord( record ) {
    const budgetObjectStore = transaction.objectStore('new_transaction');
    budgetObjectStore.add( record );
 }
+
+function uploadBudget() {
+   const transaction = db.transaction(["new_transaction"], "readwrite");
+   const budgetObjectStore = transaction.objectStore("new_transaction");
+   const getAll = budgetObjectStore.getAll();
+
+   getAll.onsuccess = function() {
+      if (getAll.result.length > 0) {
+         fetch("/api/transaction",
+            {
+               method: "POST",
+               body: JSON.stringify(getAll.result),
+               headers: {
+                  Accept: "application/json, text/plain, */*",
+                  "Content-Type": "application/json"
+               }
+            }
+         )
+            .then(response => response.json())
+            .then(serverResponse => {
+               if(serverResponse.message) {
+                  throw new Error(serverResponse);
+               }
+
+               const transaction = db.transaction(["new_transaction"], "readwrite");
+               const budgetObjectStore = transaction.objectStore("new_transaction");
+               budgetObjectStore.clear();
+            })
+            .catch(err => {
+               console.error(err);
+            });
+      }
+   }
+}
+
+window.addEventListener("online", uploadBudget);
